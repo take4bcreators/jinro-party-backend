3a689c64937da1cc13abe40ad1b71cd2e2b93211
hdeg <76611318+exit1991@users.noreply.github.com>
2023-10-12 17:50:13 +0900

    Spring Boot プロジェクト作成

---
+++ src/main/java/com/extensionlab/jinropartybackend/JinroPartyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.extensionlab.jinropartybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinroPartyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinroPartyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/extensionlab/jinropartybackend/JinroPartyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extensionlab.jinropartybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinroPartyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
